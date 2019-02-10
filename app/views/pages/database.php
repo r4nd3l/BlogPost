1b0729fa8f9df912fcb8a5fe27e53b8514d0d600
matemolnar88 <matemolnar88@gmail.com>
2019-02-10 16:05:23 +0100

    Database style update

--- app/views/pages/database.php
+++ app/views/pages/database.php
@@ -1,7 +1,10 @@
-<!-- Include the db_init.php and change the text according to the current notification! -->
 <?php require APPROOT . '/views/inc/header.php'; ?>
-  <h1><?php echo $data['title']; ?></h1>
-  <p><?php echo $data['description']; ?></p>
-  <p>In order to use the Register and Login function, the database have to be setup!</p>
+  <div class="jumbotron jumbotron-fluid rounded text-center">
+    <div class="container">
+      <h1 class="display-3"><i class="fas fa-database text-dark"></i> <?php echo $data['title']; ?></h1>
+      <p class="lead"><?php echo $data['description']; ?> is done! - Now You can <a class="text-success" href="<?php echo URLROOT; ?>/users/register">Register</a> and then <a class="text-success" href="<?php echo URLROOT; ?>/users/login">Login</a></p>
+    </div>
+  </div>
+  <!-- Include the db_init.php and change the text according to the current notification! -->
   <p>Click here to <a href="<?php echo URLROOT; ?>/db_init.php" target="_blank">proceed</a></p>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
